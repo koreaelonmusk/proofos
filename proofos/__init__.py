8fbef2102a6d86bfc7e617ff81bb785ca4835636
elonmusk <44775845+koreaelonmusk@users.noreply.github.com>
2026-08-21 16:30:04 +0900

    feat: add ProofOS package exports

---
+++ proofos/__init__.py
@@ -0,0 +1,8 @@
+from .verifier import Evidence, VerificationResult, VerificationStatus, verify_completion
+
+__all__ = [
+    "Evidence",
+    "VerificationResult",
+    "VerificationStatus",
+    "verify_completion",
+]
